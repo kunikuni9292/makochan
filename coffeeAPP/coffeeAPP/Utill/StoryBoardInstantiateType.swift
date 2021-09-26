581c1d0b2490074cea6dff8f289d222bd61890aa
くにくに <kunitatsu427@gmail.com>
2021-09-26 10:07:09 +0900

    utillFaile 追加

---
+++ coffeeAPP/coffeeAPP/Utill/StoryBoardInstantiateType.swift
@@ -0,0 +1,13 @@
+//
+//  StoryBoardInstantiateType.swift
+//  Form
+//
+//  Created by 崎原邦達 on 2021/08/25.
+//
+
+import UIKit
+
+public enum StoryBoardInstantiateType {
+    case initial
+    case identifier(String)
+}
