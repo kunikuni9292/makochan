33c94bac941c84431c7d064edb21ca3ce02b36e0
大野誠人 <>
2021-10-31 17:50:27 +0900

    ipsum　app作成

---
+++ ipusum app/ipusum app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ipusum app
+//
+//  Created by 大野誠人 on 2021/10/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
